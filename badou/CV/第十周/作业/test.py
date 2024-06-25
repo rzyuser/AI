94b40bc167ab099da1942982079cf3bcad5e189a
v_renzeyu <Rzy091621>
2024-06-25 18:57:18 +0800

    相关配置

---
+++ badou/CV/第十周/作业/test.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import models, layers, utils
+import matplotlib.pylab as plt
+import cv2, os
+
+# 加载数据
+(train_img, train_label), (test_img, test_label) = mnist.load_data()
+
+# 查看测试集中数据
+img_list = test_img[5:12]
+for i in range(len(img_list)):
+    cv2.imwrite(f'../../imgs/test_img/digit{i}.png', img_list[i])
