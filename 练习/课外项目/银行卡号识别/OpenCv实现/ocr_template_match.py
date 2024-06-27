61152acc9b48d3f34fca8c6e780b3807cac0e1df
v_renzeyu <Rzy091621>
2024-06-27 19:15:38 +0800

    相关配置

---
+++ 练习/课外项目/银行卡号识别/OpenCv实现/ocr_template_match.py
@@ -0,0 +1,132 @@
+from imutils import contours
+import numpy as np
+import argparse
+import cv2
+import myutils
+
+if __name__ == '__main__':
+    # 读取样板图
+    img = cv2.imread('../images/ocr_a_reference.png')
+    # 进行灰度化
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # 进行二值化
+    ref = cv2.threshold(img_gray, 10, 255, cv2.THRESH_BINARY_INV)[1]
+    # cv2.imshow('ref', ref)
+    # 轮廓检测
+    ref_, refCnts, hierarchy = cv2.findContours(ref.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # 展示检测效果
+    cv2.drawContours(img, refCnts, -1, (0, 0, 255), 3)
+    # cv2.imshow('img', img)
+    # 对检测出来的轮廓进行排序，以方便知道这个轮廓代表的是几 reverse=False 升序
+    refCnts = myutils.sort_contours(refCnts, method="left-to-right")[0]  # 排序，从左到右，从上到下
+    digits = {}
+    # 将轮廓与对应的值相匹配
+    digits = {}
+    # 遍历每一个轮廓
+    for (i, c) in enumerate(refCnts):
+        # 计算外接矩形并且resize成合适大小
+        (x, y, w, h) = cv2.boundingRect(c)
+        roi = ref[y:y + h, x:x + w]
+        roi = cv2.resize(roi, (57, 88))
+
+        # 每一个数字对应每一个模板
+        digits[i] = roi
+    # 初始化卷积核
+    rectKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 3))
+    sqKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+    # 读取输入图像，预处理
+    image = cv2.imread('../images/credit_card_03.png')
+    image = myutils.resize(image, width=300)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # 礼帽操作，突出更明亮的区域
+    tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+    gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0,  # ksize=-1相当于用3*3的
+                      ksize=-1)
+    gradX = np.absolute(gradX)
+    (minVal, maxVal) = (np.min(gradX), np.max(gradX))
+    gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
+    gradX = gradX.astype("uint8")
+    # 通过闭操作（先膨胀，再腐蚀）将数字连在一起
+    gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+    # THRESH_OTSU会自动寻找合适的阈值，适合双峰，需把阈值参数设置为0
+    thresh = cv2.threshold(gradX, 0, 255,
+                           cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    # 再来一个闭操作
+    thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
+
+    # 计算轮廓
+
+    thresh_, threshCnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                                      cv2.CHAIN_APPROX_SIMPLE)
+
+    cnts = threshCnts
+    cur_img = image.copy()
+    cv2.drawContours(cur_img, cnts, -1, (0, 0, 255), 3)
+    locs = []
+
+    # 遍历轮廓
+    for (i, c) in enumerate(cnts):
+        # 计算矩形
+        (x, y, w, h) = cv2.boundingRect(c)
+        ar = w / float(h)
+
+        # 选择合适的区域，根据实际任务来，这里的基本都是四个数字一组
+        if ar > 2.5 and ar < 4.0:
+
+            if (w > 40 and w < 55) and (h > 10 and h < 20):
+                # 符合的留下来
+                locs.append((x, y, w, h))
+
+    # 将符合的轮廓从左到右排序
+    locs = sorted(locs, key=lambda x: x[0])
+    output = []
+
+    # 遍历每一个轮廓中的数字
+    for (i, (gX, gY, gW, gH)) in enumerate(locs):
+        # initialize the list of group digits
+        groupOutput = []
+
+        # 根据坐标提取每一个组
+        group = gray[gY - 5:gY + gH + 5, gX - 5:gX + gW + 5]
+        # 预处理
+        group = cv2.threshold(group, 0, 255,
+                              cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        # 计算每一组的轮廓
+        group_, digitCnts, hierarchy = cv2.findContours(group.copy(), cv2.RETR_EXTERNAL,
+                                                        cv2.CHAIN_APPROX_SIMPLE)
+        digitCnts = contours.sort_contours(digitCnts,
+                                           method="left-to-right")[0]
+
+        # 计算每一组中的每一个数值
+        for c in digitCnts:
+            # 找到当前数值的轮廓，resize成合适的的大小
+            (x, y, w, h) = cv2.boundingRect(c)
+            roi = group[y:y + h, x:x + w]
+            roi = cv2.resize(roi, (57, 88))
+            # 计算匹配得分
+            scores = []
+            # 在模板中计算每一个得分
+            for (digit, digitROI) in digits.items():
+                # 模板匹配
+                result = cv2.matchTemplate(roi, digitROI,
+                                           cv2.TM_CCOEFF)
+                (_, score, _, _) = cv2.minMaxLoc(result)
+                scores.append(score)
+
+            # 得到最合适的数字
+            groupOutput.append(str(np.argmax(scores)))
+
+        # 画出来
+        cv2.rectangle(image, (gX - 5, gY - 5),
+                      (gX + gW + 5, gY + gH + 5), (0, 0, 255), 1)
+        cv2.putText(image, "".join(groupOutput), (gX, gY - 15),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
+
+        # 得到结果
+        output.extend(groupOutput)
+
+    # 打印结果
+    # print("Credit Card Type: {}".format(FIRST_NUMBER[output[0]]))
+    print("Credit Card #: {}".format("".join(output)))
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
